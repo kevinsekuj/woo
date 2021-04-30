230513b3b39c0da3486117097d08f22d0808e3b9
kevin sekuj <kevinsekuj@gmail.com>
2021-04-29 20:14:30 -0400

    added User ref to db schemas

--- models/review.js
+++ models/review.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const siteReviewSchema = new Schema({
 	body: String,
 	rating: Number,
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: "User", // ref to author via User instance
+	},
 });
 
 module.exports = mongoose.model("Review", siteReviewSchema);
