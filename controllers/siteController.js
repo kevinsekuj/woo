db8bf00b5b8ea2a132bc97c9d834110f0d032012
kevin sekuj <kevinsekuj@gmail.com>
2021-04-30 23:00:16 -0400

    add cloudinary image hosting with CRUD

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,4 +1,5 @@
 const Site = require("../models/sites");
+const { cloudinary } = require("../utilities/cloudinary");
 
 module.exports.index = async (req, res) => {
 	const sites = await Site.find({});
@@ -12,6 +13,12 @@ module.exports.addPage = (req, res) => {
 module.exports.create = async (req, res) => {
 	const newSite = new Site(req.body.site);
 	newSite.author = req.user._id;
+	// map over array from req.files object created by multer and create of array of objects
+	// containing image file path and filename, adding them onto new Site creation
+	newSite.images = req.files.map(file => ({
+		url: file.path,
+		filename: file.filename,
+	}));
 
 	await newSite.save();
 	req.flash("success", "Success! Site created");
@@ -52,13 +59,38 @@ module.exports.editForm = async (req, res) => {
 
 module.exports.editSite = async (req, res) => {
 	const { id } = req.params;
-
 	const site = await Site.findByIdAndUpdate(id, { ...req.body.site });
+
+	const images = req.files.map(file => ({
+		url: file.path,
+		filename: file.filename,
+	}));
+
+	// push spread array elements to images array of Site object
+	site.images.push(...images);
+	await site.save();
+
+	// if deleteImages in request, pull from images array all images
+	// where filename in deleteImages array (from ejs form)
+	if (req.body.deleteImages) {
+		const { deleteImages } = req.body;
+
+		await site.updateOne({
+			$pull: { images: { filename: { $in: deleteImages } } },
+		});
+
+		// iterate through to-be deleted elements of deleteImages array and
+		// to delete assets from Cloudinary storage
+		for (let filename of deleteImages) {
+			await cloudinary.uploader.destroy(filename);
+		}
+	}
 
 	if (!site) {
 		req.flash("error", "Sorry, this site doesn't exist.");
 		res.redirect("/sites");
 	}
+
 	req.flash("success", "Successfully updated tourist site!");
 	res.redirect(`/sites/${id}`);
 };
