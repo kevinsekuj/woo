677ca986d34a52d2a0ebe44c5e0d8a7659523765
kevin sekuj <kevinsekuj@gmail.com>
2021-04-30 23:01:35 -0400

    routes for image uploading and hosting

--- routes/sites.js
+++ routes/sites.js
@@ -3,20 +3,35 @@ const router = express.Router();
 const catchAsync = require("../utilities/asyncError");
 const { isLoggedIn, authorize, validate } = require("../utilities/middleware");
 const sites = require("../controllers/siteController");
+const multer = require("multer");
+
+const { storage } = require("../utilities/cloudinary");
+const upload = multer({ storage });
 
 // CRUD for sites w/ controllers to separate route requests
 
 router
 	.route("/")
 	.get(catchAsync(sites.index))
-	.post(isLoggedIn, validate, catchAsync(sites.create));
+	.post(
+		isLoggedIn,
+		upload.array("image"),
+		validate,
+		catchAsync(sites.create)
+	);
 
 router.get("/add", isLoggedIn, sites.addPage);
 
 router
 	.route("/:id")
 	.get(catchAsync(sites.displaySite))
-	.put(isLoggedIn, authorize, validate, catchAsync(sites.editSite))
+	.put(
+		isLoggedIn,
+		authorize,
+		upload.array("image"),
+		validate,
+		catchAsync(sites.editSite)
+	)
 	.delete(isLoggedIn, authorize, catchAsync(sites.deleteSite));
 
 router.get("/:id/edit", isLoggedIn, authorize, catchAsync(sites.editForm));
