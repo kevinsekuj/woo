d93e6aacba4d99537ac71d420d392bfce5d9dd6e
kevin sekuj <kevinsekuj@gmail.com>
2021-05-04 17:50:39 -0400

    add error msgs to server side validation with joi

--- utilities/reviewSchema.js
+++ utilities/reviewSchema.js
@@ -2,7 +2,9 @@ const Joi = require("joi");
 
 const reviewSchema = Joi.object({
 	review: Joi.object({
-		body: Joi.string().required(),
+		body: Joi.string().required().max(500).messages({
+			"string.max": "Please keep reviews to under 500 characters.",
+		}),
 		rating: Joi.number().required().max(5).min(0),
 	}).required(),
 });
--- utilities/schemas.js
+++ utilities/schemas.js
@@ -2,10 +2,22 @@ const Joi = require("joi");
 
 const siteSchema = Joi.object({
 	site: Joi.object({
-		name: Joi.string().required(),
-		price: Joi.number().required().min(0),
-		description: Joi.string().required(),
-		location: Joi.string().required(),
+		name: Joi.string().required().max(40).messages({
+			"string.max":
+				"The title must be less than or equal to 40 characters.",
+		}),
+		price: Joi.number().required().min(0).messages({
+			// this should never be reached, but just in case
+			"number.min":
+				"You must enter a price. If the location is free, enter 0.",
+		}),
+		description: Joi.string().required().max(350).messages({
+			"string.max":
+				"There is a 350 character limit on location descriptions.",
+		}),
+		location: Joi.string().required().max(60).messages({
+			"string.max": "Please keep the location to under 30 characters.",
+		}),
 		// image: Joi.string(),
 	}).required(),
 	deleteImages: Joi.array(), // array containing checked img filepaths
