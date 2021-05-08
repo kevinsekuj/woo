6ac3bbb63394318b203f61bc1959d362525999d3
Kevin <68984611+kevinsekuj@users.noreply.github.com>
2021-05-07 22:46:04 -0400

    Update map.js

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,16 +1,17 @@
 // Initialize and add the map
 
 function initMap(latitude, longitude) {
-	const worcester = { lat: 42.269, lng: -71.802 };
-	const map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 13,
-		center: worcester,
-	});
-	// this sucks but couldn't get ejs to pass vars to scripts
 	const coord = document.querySelector(".coord").id.split(",");
 	let [strLat, strLon] = coord;
 	let lat = parseFloat(strLat);
 	let lon = parseFloat(strLon);
+
+	const center = { lat: lat, lng: lon };
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 18,
+		center: center,
+	});
+
 	setTimeout(() => {
 		const marker = new google.maps.Marker({
 			position: {
