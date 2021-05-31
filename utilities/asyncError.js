551f5500f78685d5655e2a99e1904f8e976225ea
Kevin <68984611+kevinsekuj@users.noreply.github.com>
2021-05-31 01:21:42 -0400

    Update asyncError.js

--- utilities/asyncError.js
+++ utilities/asyncError.js
@@ -1,6 +1,5 @@
 // takes function as parameter and returns a new function which
 // executes func, catching any errors and passes to next middleware
-// as alternative to try/catch block spam
 
 const asyncWrapper = func => {
 	return (req, res, next) => {
